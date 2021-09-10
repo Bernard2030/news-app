6de80afe368ce0d99fccec5d08ceeebf1e3622e2
Bernard Opiyo <brobernard.254@gmail.com>
2021-09-11 00:25:58 +0300

    update config content

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 class Config:
 
-    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    MOVIE_API_BASE_URL ='https://newsapi.org/v2/top-headlines/sources?apiKey=e984567f65124d82bdb23cfc40ce31b8'
+    MOVIE_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
