c5c7f2b04cc4a35709a81747ad8449d91921d655
Bernard Opiyo <brobernard.254@gmail.com>
2021-09-13 07:54:19 +0300

    comment news.py

--- app/news.py
+++ app/news.py
@@ -1,32 +1,32 @@
-class News:
-    """
-    News class to define news objects
-    """
+# class News:
+#     """
+#     News class to define news objects
+#     """
 
-    def __init__(self, id, name, description, url,  category,  country, ):
-        """
-        Getting news objects
-        """
-        self.id = id
-        self.name = name
-        self.description = description
-        self.url = url
-        self.category = category
-        self.country = country
+#     def __init__(self, id, name, description, url,  category,  country, ):
+#         """
+#         Getting news objects
+#         """
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.url = url
+#         self.category = category
+#         self.country = country
         
 
-class Articles:
-    """
-    Article class that returns details of an article
-    """        
-    def __init__(self,title, author,description, url, urlToImage, publishedAt, content):
-        self.title = title
-        self.author = author
-        self.description = description
-        self.url = url
-        self.urlToImage = urlToImage
-        self.publishedAt = publishedAt
-        self.content = content
+# class Articles:
+#     """
+#     Article class that returns details of an article
+#     """        
+#     def __init__(self,title, author,description, url, urlToImage, publishedAt, content):
+#         self.title = title
+#         self.author = author
+#         self.description = description
+#         self.url = url
+#         self.urlToImage = urlToImage
+#         self.publishedAt = publishedAt
+#         self.content = content
         
 
        
